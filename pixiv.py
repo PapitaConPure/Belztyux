3c25acb76fc584513bcacc32540085269a4693f4
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-03-21 02:36:12 -0300

    Tratar directorios relativos

--- pixiv.py
+++ pixiv.py
@@ -2,6 +2,12 @@ from pixivpy3 import *
 import imgur
 import os
 
+#Trabajar con directorio relativo
+dirname = os.path.dirname(__file__)
+print(os.getcwd())
+print(__file__)
+print(dirname)
+
 #Autorizar API de pixiv
 pixiv_api = AppPixivAPI()
 token = os.getenv('PIXIV_REFRESH_TOKEN')
@@ -16,9 +22,11 @@ def upload_image_to_imgur(url, image_filename='pixiv-temp'):
     '''Subir imagen de pixiv a Imgur y retornar el enlace de Imgur'''
     print(token)
     #try:
-    pixiv_api.download(url, path='pix', name=image_filename)
-    image = imgur.upload_image(os.path.join('pix', image_filename))
-    os.remove(os.path.join('pix', image_filename))
+    file_path = os.path.join(dirname, 'pix')
+    print(file_path)
+    pixiv_api.download(url, path=file_path, name=image_filename)
+    image = imgur.upload_image(os.path.join(file_path, image_filename))
+    os.remove(os.path.join(file_path, image_filename))
     return image
     #except Exception:
     #    print('Error al descargar imagen desde pixiv o subirla a Imgur')
